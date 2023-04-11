b388205836e67730df818dcc2d407cec87dac8bd
Ashik465 <helloashikul@gmail.com>
2023-04-11 16:31:49 +0600

    Statistics-page-done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import Header from '../Header/Header';
+import { useLoaderData } from 'react-router-dom';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
 
 const Statistics = () => {
+
+     const assignmentData = useLoaderData();
+     console.log(assignmentData)
+
     return (
         <div>
-            <h1>hello from Statistics</h1>
+              
+              <Header>
+
+            <h1 className=" font-bold text-3xl my-32 pb-32 text-[#1A1919] text-center">Statistics Page </h1>
+            </Header>
+   
+             <div className='text-center'><h1 className=" font-bold text-3xl  pb-32 pinky ">Area chart of Assignment Marks </h1></div>
+            
+
+
+
+        <div style={{ width: '100%', height: 300 }}>
+        
+        
+        <ResponsiveContainer>
+        
+           <AreaChart
+            width={500}
+            height={400}
+            data={assignmentData}
+            margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+           >
+                    <CartesianGrid strokeDasharray="3 3" />
+           <XAxis dataKey="assignment_no"  />
+          <YAxis dataKey="assignment_marks" />
+          <Tooltip />
+          <Area type="monotone" dataKey="assignment_marks" stroke="#8884d8" fill="#8884d8" />
+           </AreaChart> 
+        
+           </ResponsiveContainer>
+        </div>   
+
+
+
+            
         </div>
     );
 };
